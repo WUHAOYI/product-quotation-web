8c0e5f74942775117300e15d3df25dd0d67f3ff5
WHY <1620283589@qq.com>
2024-11-06 17:58:50 +0800

    add api instruction

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,13 @@
 import { get, post, put, del } from '@/utils/request'
 const baseUrl = 'http://localhost:8888'
 
+//个人信息
 export const getUserInfo = p => get(baseUrl + '/user')
 export const updateUserInfo = (p, header) => put(baseUrl + '/user', p, header)
 export const uploadVideo = (p, header) => post(baseUrl + '/video', p, header)
+//视频
 export const getVideoList = p => get(baseUrl + '/video', p)
 export const deleteVideoById = p => del(baseUrl + '/video/' + p)
-
+//上新
 export const getTime = () => get(baseUrl + '/time')
 export const updateTime = p => put(baseUrl + '/time/' + p)
